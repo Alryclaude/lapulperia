8e6df036f367c02eb0f98c951861a4a65b9873b2
Alryclaude <nolascale694@gmail.com>
2026-01-17 10:48:24 -0600

    fix: Agregar extensión .jsx en imports de share/index.js

--- client/src/components/share/index.js
+++ client/src/components/share/index.js
@@ -1,3 +1,3 @@
 // Componentes de compartir en redes sociales
-export { default as ShareButton } from './ShareButton';
-export { default as ShareButtons } from './ShareButtons';
+export { default as ShareButton } from './ShareButton.jsx';
+export { default as ShareButtons } from './ShareButtons.jsx';
